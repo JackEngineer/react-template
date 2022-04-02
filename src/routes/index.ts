8acf8888991735b7ec34f0eb38c059a777d890ea
jacklee <824477269@qq.com>
2022-04-02 11:21:30 +0800

    chore: 路由示例配置

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
+import module1 from "./modules/module1";
+import module2 from "./modules/module2";
+
 export default [
-    {
-        path: '/'
-    }
+    { path: '/', component: '@/pages/index' },
+    ...module1,
+    ...module2
 ]
